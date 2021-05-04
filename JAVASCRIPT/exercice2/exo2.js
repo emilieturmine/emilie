4e893ea6c929830082ad9577da98a0c5d9115c8a
emilieturmine <82571837+emilieturmine@users.noreply.github.com>
2021-05-04 13:18:16 +0200

    Add files via upload
    javascript 04/05 a13h

---
+++ JAVASCRIPT/exercice2/exo2.js
@@ -0,0 +1,17 @@
+var a = "100"
+var b = 100
+var c = "1.00"
+var d = true
+document.write("Ceci est une chaine de caracteres :" + a + '<br/>');
+document.write(b + '<br/>')
+var b;
+b = b - 1;
+document.write(b + '<br/>');
+document.write(c + '<br/>');
+var c;
+c = c + a;
+document.write(c + '<br/>');
+document.write(d + '<br/>');
+var d;
+d = !(d);
+document.write(d);
