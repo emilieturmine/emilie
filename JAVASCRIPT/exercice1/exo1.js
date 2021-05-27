50ef0f45ab5afcdc1b38d47574fdd89872c52913
emilieturmine <82571837+emilieturmine@users.noreply.github.com>
2021-05-27 10:02:55 +0200

    Delete JAVASCRIPT directory

--- JAVASCRIPT/exercice1/exo1.js
+++
@@ -1,5 +0,0 @@
-var nom = window.prompt("entrez votre nom");
-var prenom = window.prompt("entrez votre prénom");
-if (window.confirm("Etes vous un homme?") == true) {
-    document.write('<br/>' + "Bonjour Monsieur " + nom + " " + prenom + '<br/>' + "Bienvenue sur notre site web!");
-}
